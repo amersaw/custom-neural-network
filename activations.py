6d1dca81e03e053b416852030e81ad559d720696
Amer Sawan <amersaw@gmail.com>
2018-04-06 15:45:57 +0300

    Update the activations

--- activations.py
+++ activations.py
@@ -1,4 +1,6 @@
 from math import exp
+import numpy as np
+
 def sigmoid(v):
 	return 1.0 / (1.0 + exp(-v))
 
@@ -7,9 +9,7 @@ def sigmoid_derivative(v):
 
 
 def tanh(v):
-	import numpy as np
 	return np.tanh(v)
 
 def tanh_derivative(v):
-	import numpy as np
-	return 1- np.tanh(v)**2 
+	return 1- np.tanh(v)**2
